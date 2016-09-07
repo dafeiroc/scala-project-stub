987797ef43f1fc1c12af96e054a448a08991a475
Pengfei Zhang <pfzhang1987@gmail.com>
2016-09-07 16:05:06 +0900

    update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,9 +4,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val akka                    = "2.3.11"
-    val akkaHttp                = "1.0-RC4"
-    val akkaStream              = "1.0-RC4"
+    val akka                    = "2.4.9"
     val aspectj                 = "1.8.6"
     val logback                 = "1.1.3"
     val mysql                   = "5.1.39"
@@ -14,20 +12,22 @@ object Dependencies {
     val play                    = "2.4.1"
     val scalactic               = "2.2.5"
     val typesafeConfig          = "1.3.0"
+    val scalaTime                = "2.6.0"
+    val cats                     = "0.3.0"
+
 
     val scalaTest               = "2.2.5"
     val scalaMock               = "3.2.2"
     val scalaCheck              = "1.12.2"
-    val scalaTime                = "2.6.0"
-    val cats                     = "0.3.0"
+
   }
 
   val akkaActor               = "com.typesafe.akka"              %%  "akka-actor"                           % Versions.akka
-  val akkaHttp                = "com.typesafe.akka"              %%  "akka-http-experimental"               % Versions.akkaHttp
-  val akkaHttpCore            = "com.typesafe.akka"              %%  "akka-http-core-experimental"          % Versions.akkaHttp
-  val akkaHttpXml             = "com.typesafe.akka"              %%  "akka-http-xml-experimental"           % Versions.akkaHttp
+  val akkaHttp                = "com.typesafe.akka"              %%  "akka-http-experimental"               % Versions.akka
+  val akkaHttpCore            = "com.typesafe.akka"              %%  "akka-http-core"                       % Versions.akka
+  val akkaHttpXml             = "com.typesafe.akka"              %%  "akka-http-xml-experimental"           % Versions.akka
   val akkaSlf4j               = "com.typesafe.akka"              %%  "akka-slf4j"                           % Versions.akka
-  val akkaStream              = "com.typesafe.akka"              %%  "akka-stream-experimental"             % Versions.akkaStream
+  val akkaStream              = "com.typesafe.akka"              %%  "akka-stream"                          % Versions.akka
   val logback                 = "ch.qos.logback"                 %   "logback-classic"                      % Versions.logback
   val mysql                   = "mysql"                          %   "mysql-connector-java"                 % Versions.mysql
   val slick                   = "com.typesafe.slick"             %%   "slick"                               % Versions.slick
@@ -38,7 +38,7 @@ object Dependencies {
   val scalaTime                = "com.github.nscala-time"         %%  "nscala-time"                          % Versions.scalaTime
   val cats                     = "org.spire-math"                 %%  "cats"                                 % Versions.cats
 
-  val akkaHttpTest       = "com.typesafe.akka"              %%  "akka-http-testkit-experimental"       % Versions.akkaHttp
+  val akkaHttpTest       = "com.typesafe.akka"              %%  "akka-http-testkit"                    % Versions.akka
   val akkaTest           = "com.typesafe.akka"              %%  "akka-testkit"                         % Versions.akka
   val scalaCheck         = "org.scalacheck"                 %%  "scalacheck"                           % Versions.scalaCheck
   val scalaMock          = "org.scalamock"                  %%  "scalamock-scalatest-support"          % Versions.scalaMock
